32e85d46c6d4c2739846db809accec348147e0af
hafiznii <95267881+hafiznii@users.noreply.github.com>
2021-11-29 20:40:49 -0800

    Create Arrayloops.php

---
+++ Arrayloops.php
@@ -0,0 +1,18 @@
+<?php
+$aplikasi[1]= "gtAkademik";
+$aplikasi[2]= "gtFinansi";
+$aplikasi[3]= "gtPerizinan";
+$aplikasi[4]= "eCampuz";
+$aplikasi[5]= "eOviz";
+
+$i=1;
+
+while ($i<sizeof($aplikasi)){
+	echo $aplikasi[$i]."<br/>";
+    $i++;
+}
+
+
+
+
+?>
